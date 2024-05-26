d50f2bf9ebbae4a7bd706b8a1e9eb3846b462042
Sachink34566 <sachinkumar98504@gmail.com>
2024-05-26 20:27:03 +0530

    Create player

---
+++ SnakeAndLadder/Player.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class Player
+    {
+        public int Position { get; private set; }
+        public int DiceRollsCount { get; private set; }
+
+        public Player()
+        {
+            Position = 0;
+            DiceRollsCount = 0;
+        }
+
+        public void UpdatePosition(int move)
+        {
+            Position += move;
+            if (Position < 0)
+            {
+                Position = 0;
+            }
+            DiceRollsCount++;
+        }
+
+        public void SetPosition(int position)
+        {
+            Position = position;
+        }
+    }
+}
